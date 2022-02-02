e645cd11b68741ee0d33e0a3a7ffb4543ecce8b3
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-02 18:40:43 +0200

    Done User

--- M2HW2/User.cs
+++ M2HW2/User.cs
@@ -8,5 +8,18 @@ namespace Shop
 {
     public class User
     {
+        public User(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+        }
+
+        public string Name { get; private set; }
+        public decimal Money { get; private set; }
+        public Guid IdUser { get; } = Guid.NewGuid();
+        public void Pay(decimal money)
+        {
+            Money -= money;
+        }
     }
 }
